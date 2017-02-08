872fca0b18b3420e187ed497b05b5c0412a8bed6
achen29 <achen28@stuy.edu>
2017-02-07 22:19:10 -0500

    Add files via upload

---
+++ 01Recursion/Recursion.java
@@ -0,0 +1,24 @@
+public class Recursion{
+    public static String name(){
+	return "Alvin Chen";
+    }
+    public static double sqrt(double n){
+	return sqrthelp(n,1);
+    }
+    public static boolean closeEnough(double m, double n){
+	if (m == 0 && n ==0){
+	    return true;
+	}
+	if (m == 0 || n == 0){
+	    return false;
+	}
+	return ((m - n)/m <= 0.00001 && (m-n)/m >= -0.00001);
+    }
+    public static double sqrthelp(double n, double guess){
+	double newguess = (n/guess + guess)/2;
+	if (closeEnough(newguess, guess)){
+	    return newguess;
+	}
+	return sqrthelp(n,newguess);
+    }
+}
